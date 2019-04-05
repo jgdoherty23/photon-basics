e8bfbbddaf5b326838e4654c804bf6e7749ec070
Jackson Doherty <jgdoherty23@gmail.com>
2019-04-05 16:39:04 -0700

    4 - Game Manager & Levels

--- Assets/Jackson/Scripts/Photon/GameManager.cs
+++ Assets/Jackson/Scripts/Photon/GameManager.cs
@@ -12,6 +12,26 @@ namespace Jackson.Photon
     {
         #region Photon Callbacks
 
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                LoadArena();
+            }
+        }
+
         /// <summary>
         /// Called when the local player left the room. We need to load the launcher scene.
         /// </summary>
@@ -28,6 +48,21 @@ namespace Jackson.Photon
         public void LeaveRoom()
         {
             PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount); // convention
         }
 
         #endregion
